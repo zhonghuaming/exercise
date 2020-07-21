c5ca17bb66a8a306fe1445f3a9270756a1702484
zhonghuaming <zhonghuaming@shencom.cn>
2020-07-21 23:45:14 +0800

    about springboot

---
+++ src/main/java/cn/huaming/springboot/errorhandler/AcmeController.java
@@ -0,0 +1,22 @@
+package cn.huaming.springboot.errorhandler;
+
+import cn.huaming.service.impl.UserService;
+import cn.huaming.service.impl.UserServiceB;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/acme")
+@Slf4j
+public class AcmeController {
+
+    @RequestMapping(value = "/test")
+    @ResponseBody
+    public String go(){
+        throw new RuntimeException();
+    }
+
+}
